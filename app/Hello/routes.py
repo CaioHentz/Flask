b820f6ef3cb135da80cb8e215d6d4f7e4cb5259d
Silva <caio.silva01@sap.com>
2025-09-09 17:38:55 -0300

    Major changes

--- app/Hello/routes.py
+++ app/Hello/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from ..models import User, db
 
 hello_bp = Blueprint('hello', __name__)
@@ -8,6 +8,27 @@ def index():
     usuarios = User.query.all()
     return render_template('index.html', usuarios=usuarios)
 
-@hello_bp.route('/sobre')
-def sobre():
-    return render_template('sobre.html')
+@hello_bp.route('/novoUsuario', methods = ['POST'])
+def novoUsuario():
+    username = request.form['nome-usuario']
+
+    novo_usuario = User(username = username)
+    db.session.add(novo_usuario)
+    db.session.commit()
+    return redirect('/')
+
+@hello_bp.route('/removerUsuario/<int:usuario_id>', methods=['POST'])
+def removerUsuario(usuario_id):
+    usuario = User.query.get(usuario_id)
+    if usuario:
+        db.session.delete(usuario)
+        db.session.commit()
+    return redirect(url_for('hello.index'))
+
+@hello_bp.route('/editarUsuario/<int:usuario_id>', methods=['POST'])
+def editarUsuario(usuario_id):
+    usuario = User.query.get(usuario_id)
+    if usuario:
+        usuario.username = request.form['nome_usuario']
+        db.session.commit()
+    return redirect(url_for('hello.index'))
